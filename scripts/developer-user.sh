0951845edba3043ee0ac609754bbe4aeb0062cb9
Michael Schnell <1856319+michael-schnell@users.noreply.github.com>
2018-10-19 08:30:51 +0200

    Manage Docker as a non-root user

--- scripts/developer-user.sh
+++ scripts/developer-user.sh
@@ -3,3 +3,5 @@
 adduser --quiet --disabled-password --shell /bin/bash --home /home/developer --gecos "Developer" developer
 echo "developer:developer" | chpasswd
 sudo adduser developer sudo
+sudo groupadd docker
+sudo usermod -aG docker developer

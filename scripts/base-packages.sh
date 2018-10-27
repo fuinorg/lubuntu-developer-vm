7480da4ff2a0dd2121ce4ce1e961161f08d14540
Michael Schnell <1856319+michael-schnell@users.noreply.github.com>
2018-10-27 12:17:42 +0200

    Update base-packages.sh

--- scripts/base-packages.sh
+++ scripts/base-packages.sh
@@ -10,7 +10,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 apt-get install -y leafpad galculator chromium-browser openjdk-8-jdk openjfx maven file-roller unzip git git-flow docker.io python-pip
-sudo update-java-alternatives --set java-1.8.0-openjdk-amd6
+update-java-alternatives --set java-1.8.0-openjdk-amd6
 
 COMPOSE_FILE=/usr/local/bin/docker-compose
 COMPOSE_URL=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/docker/compose/releases/latest)

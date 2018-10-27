d37c2705a7dffca26ef833679aa10ae90babc0fd
Michael Schnell <1856319+michael-schnell@users.noreply.github.com>
2018-10-27 16:23:28 +0200

    Update developer-user.sh

--- scripts/developer-user.sh
+++ scripts/developer-user.sh
@@ -3,5 +3,3 @@
 adduser --quiet --disabled-password --shell /bin/bash --home /home/developer --gecos "Developer" developer
 echo "developer:developer" | chpasswd
 sudo adduser developer sudo
-sudo groupadd docker
-sudo usermod -aG docker developer
